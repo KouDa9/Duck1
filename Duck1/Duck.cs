ee2c76873720752fb41ad3fb612a5f32dade153a
argentoff1 <mparinos56@gmail.com>
2022-01-19 12:46:20 +0300

    Добавьте файлы проекта.

---
+++ Duck1/Duck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duck1
+{
+    abstract class Duck
+    {
+        public string quack()
+        {
+            return "quack" + this.GetType();
+        }
+
+        public string swim()
+        {
+            return "swim" + this.GetType();
+        }
+
+        public abstract string display();
+    }
+
+    class MallarDuck : Duck
+    {
+        public override string display()
+        {
+            return "display" + this.GetType();
+        }
+    }
+    class RedheadDuck : Duck
+    {
+        public override string display()
+        {
+            return "display" + this.GetType();
+        }
+    }
+}
